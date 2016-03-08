3ed1a6ca383c65b06e7626cf78def64b27633fa7
Jan Krag <jankrag@Jans-MacBook-Pro.local>
2016-03-08 16:25:11 +0100

    New message

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main (String [] args) {
 		System.out.println("Hello Dockeristas!");
-		System.out.println("Thanks for behaving nicely :-)");
+		System.out.println("Thanks for behaving so nicely :-)");
 	}
 }
